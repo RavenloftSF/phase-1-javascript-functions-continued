bcdf6bd92e2eeb09599eca45fef3720bae764905
JosephBorrelli <josephborrelli80@gmail.com>
2023-03-16 13:57:13 -0400

    edited

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+
+function saturdayFun(totallyAwesome="roller-skate") {
+    return `This Saturday, I want to ${totallyAwesome}!`
+    }
+    saturdayFun()
+
+let mondayWork = function(workingForTheWeekend="go to the office"){
+    return `This Monday, I will ${workingForTheWeekend}.` 
+       }
+       mondayWork()
+
+function wrapAdjective (fancy="*") {
+    return function(stringOne="special"){
+    return `You are ${fancy}${stringOne}${fancy}!`
+        }
+      }
